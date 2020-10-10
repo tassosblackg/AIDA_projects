4843e866d2554d40f6c397be56e9c435d17546e4
tassosblackg <soylis.k@gmail.com>
2020-10-11 02:39:26 +0300

    finish reading without ranges..no tested..

--- AIDA04/project_w1/handle_mps_file.py
+++ AIDA04/project_w1/handle_mps_file.py
@@ -2,17 +2,21 @@
 # convert to dictioneries
 
 import argparse
-from pysnooper import snoop
-# A,b,c,E_qin = [],[],[],[],[]
+import numpy as np
+# from pysnooper import snoop
 
-@snoop('mps2data_.log')
+# @snoop('mps2data_.log')
 def mps2data(file):
     '''
     Read a .mps and return matrices with data
     '''
     in_ROWS_section = False
+    in_COLS_section = False
+    in_RHS_section = False
     Rtypes, Rnames, Cnames, Rindx = [],[],[],[]
-
+    Rows = dict()
+    Bounds = dict()
+    A,c,b =[],[],[]
     with open(file,'r') as f:
         for l in f:
             if( (len(l) == 0) or (l[:1] == '*') ):
@@ -25,28 +29,72 @@ def mps2data(file):
                 in_ROWS_section = True
                 # pass
             elif(in_ROWS_section and l[:7] != 'COLUMNS'): # => inside ROWS section
-                Rtypes.append(l[:4].strip()) # Get all types ['N','L','E','G'] per RowName
-                Rnames.append(l[4:13].strip()) # Get RowsNames
+                field1 = l[:4].strip() # Constraint Type
+                field2 = l[4:13].strip() # Row Name -indx
+                if(field1 == 'N'): #objective func
+                    obj_func_name = field2 #name of objective fuction
+                else:
+                    Rows[int(field2)] = field1 # create Dictionary {key=Row_i, value in ['E','G','L'] }
 
             elif(l[:7]=='COLUMNS'): # => inside COLUMNS section
                 in_ROWS_section = False # outside the ROWS section
                 in_COLS_section = True  # inside COLUMNS section
 
             elif(in_COLS_section and l[:7] != 'COLUMNS'):
-                Cnames.append(l[4:13].strip())
-                Rindx.append(l[14:23].strip())
-
-            elif(in_COLS_section and l[:4] != 'RHS'):
-                in_COLS_section = False
+                col_indx = int(l[5:13].strip()) # Col_name drop first_letter take number -> indx
+                field4 = float(l[24:37].strip()) # Get value from field 4, convert to float
+                if(l[14:23].strip() == obj_func_name):
+                    c[col_indx] = field4
+                else: # row_name is not objective_function
+                    row_indx = int(l[15:23].strip()) # Row_name -> indx
+                    A[row_indx,col_indx] = field4
+                    if(len(l) > 40): # there is field5 field6
+                        row_indx = int(l[40:48].strip()) # field5 row_idx
+                        A[row_indx,col_indx] = float(l[50:62].strip()) #field6 value
 
             elif(l[:4] == 'RHS'):
-                pass
+                in_COLS_section = False
+                in_RHS_section = True
+
+            elif(in_RHS_section and l[:4] != 'RHS'):
+                in_RHS_section = False
+                row_indx = int(l[15:23].strip()) # row_indx
+                b[row_indx] = float(l[24:36].strip()) # get value
+                if(len(l) > 40):
+                    row_indx = int(l[40:48].strip()) # field 5 -> row_indx
+                    b[row_indx] = float(l[50:62].strip()) # field 6 value
+            elif(l[:4] == 'RANGES'):
+                in_RHS_section = False
+                ''' ignore RANGES'''
+            elif(l[:6] == 'BOUNDS'):
+                in_BOUNDS_section = True
+            elif(in_BOUNDS_section and l[:6] != 'BOUNDS'):
+                col_indx = int(l[15:23].strip()) # get column name -> indx
+                values = [l[:4].strip(),l[25:37].strip()] # create a list contains [Type, Value] for each Bound
+                Bounds[col_indx] = values
 
             elif(l[:4]=='ENDATA'):
+                in_BOUNDS_section = False
                 print("EOF reached..\n")
-    f.close()
 
-    return (problem_name,Rtypes,Rnames,Cnames, Rindx)
+        f.close()
+
+#------------------ Convert ['E', 'G', 'L'] -> [0,1,-1] ----------------------------------------------------------------------------------
+    for key in Rows:
+        if (Rows[key] == 'E'):
+            Rows[key] = 0
+        elif (Rows[key] == 'G'):
+            Rows[key] = 1
+        elif (Rows[key] == 'L'):
+            Rows[key] = -1
+        else:
+            print("Error with Rows constraint Type see Row.keys()..\n")
+
+#------------------------------- RHS vector from 1xN to Nx1 format Transpose--------------------------------------------
+    min_max = 1
+    return (problem_name,np.array(b).T,np.array(A),np.array(c),min_max, Rows, Bounds)
+
+    # return (problem_name,Rtypes,Rnames,Cnames, Rindx)
 
 def data2mps(file):
     f = open(file,'r')
@@ -65,11 +113,7 @@ def parserM():
     args=parser.parse_args()
     print(args)
     if (args.read):
-        name,Rt,Rn,Cn,Ri = mps2data(args.input_file)
-        print(name)
-        print(Rt,'\n',Rn)
-        print(Cn,'\n',Ri)
-
+        mps2data(args.input_file)
     else:
         data2mps(args.input_file)
 
