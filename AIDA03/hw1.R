ab1cc453d4ca79891edef82585e9b4de71ddb9d1
tassosblackg <soylis.k@gmail.com>
2020-10-23 11:31:32 +0300

    publish v0.9 --check 13b

--- AIDA03/hw1.R
+++ AIDA03/hw1.R
@@ -20,44 +20,61 @@ a=head(iris,10) # 1-10
 tail(a,5) # apo tis 10 tis teleytaies 5 -> 6:10
 
 # 2. Εμφανίστε όλα τα λουλούδια versicolor με Sepal.length > 6
-
-
+versicolor <- filter(iris,Species=="versicolor",Sepal.Length >6)
+versicolor
 # 3. Εμφανίστε όλα τα λουλούδια που έχουν Petal.length μεγαλύτερο από το μέσο Petal.length
-
+m <- mean(iris$Petal.Length)
+flower3 <- filter(iris,Petal.Length> m)
 
 # 4. Εμφανίστε όλες τις στήλες εκτός του Species.
-
-
+flower4 <- subset(iris,select = -c(Species))
+flower4
 # 5. Εμφανίστε τις στήλες που αρχίζουν από 'S'.
-
-
+flower5<-select(iris,starts_with("S"))
+flower5
 # 6. Εμφανίστε τις μεταβλητές που περιέχουν το αλφαριθμητικό "Len".
-
+indx <- grepl('Len', colnames(iris)) # βρες τους δεικτες των columns που εχουν το "Len"
+names(iris[indx])
 
 # 7. Μετονομάστε τη στήλη Species σε Type.
-
-
+# colnames(iris)[colnames(iris) == "Species"] <- "Type"
+iris<-iris %>% rename(Type = Species)
+head(iris)
 # 8. Ταξινομήστε το αρχείο ως προς Petal.Length (desc) και μετά ως προς Petal.Width (asc)
-
-
+flower8 <- iris %>% arrange(desc(Petal.Length))
+flower8
+flower8b <-iris %>% arrange(desc(Petal.Width))
+flower8b
 # 9. Προσθέστε δυο στήλες με τα αθροίσματα των μηκών και των πλατών
-
+iris$LengthSum<-iris$Sepal.Length + iris$Petal.Length
+iris$WidthSum<-iris$Sepal.Width + iris$Petal.Width
 
 #10. Υπολογίστε το άθροισμα της κάθε στήλης (hint: δείτε την summarise_all())
-
+iris_new <- as_tibble(iris)
+#summarise_all(iris_new,)
+iris_new %>%
+  select(-Type) %>%
+  summarise_all(sum) #??
 
 #11. Για κάθε είδος (Species) υπολογίστε τα min, max, mean, median του Petal.Length
 #    (hint: δείτε την summarise_at())
-
+iris_new %>%
+  group_by(Type) %>% 
+  summarize_at(vars(Petal.Length), funs(min, max, mean, median))
 
 # Συνεχίζουμε με την tidyr
-
+library(tidyr)
 #12. Μεταβλητές: από στήλες σε ζεύγη τύπου (κλειδί, τιμή)
 #    Πολλές φορές μια μεταβλητή (variable) καταλαμβάνει πολλές στήλες. Για παράδειγμα, στο dataset iris,
 #    μπορεί να επιθυμούμε να θεωρήσουμε τα Sepal.Length, Sepal.Width, Petal.Length και Petal.Width ως
 #    παραλλαγές της μοναδικής μεταβλητής measure. Μετασχηματίστε το iris στο iris2 με την παρακάτω δομή:
 
-# head(iris2)
+iris_drop_sums <- iris %>% select(-LengthSum,-WidthSum)
+
+  iris2 <- iris_drop_sums %>%
+              gather("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width", key="measure",value="value")
+
+head(iris2)
 # Species      measure value
 # 1  setosa Sepal.Length   5.1
 # 2  setosa Sepal.Length   4.9
@@ -77,16 +94,24 @@ tail(a,5) # apo tis 10 tis teleytaies 5 -> 6:10
 #    (α) να δίναμε πρώτα ένα μοναδικό ID σε κάθε λουλούδι και μετά να προχωρούσαμε σε αυτό που ζητούσε το
 #    ερώτημα 12. Συνεπώς, προσθέστε μια στήλη με μοναδικό ID (1,2,3...,150) σε κάθε εγγραφή του iris πριν
 #    δημιουργήσετε το iris2.
+iris_with_ID <- mutate(iris_drop_sums,ID=row_number())
+iris2a <- iris_with_ID %>%
+  gather("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width", key="measure",value="value")
+
 #    (β) εναλλακτικά, αριθμήστε τα 50 στιγμιότυπα του κάθε είδος λουλουδιού από το 1 ως το 50.
 #    Στο πρώτο σενάριο το κλειδί είναι το πεδίο ID που προσθέσαμε, ενώ στο δεύτερο σενάριο, κλειδί είναι ο
 #    συνδυασμός (ID, species). (hint: δείτε τί κάνει η συνάρτηση rep).
-
+iris_with_ID_2<-mutate(iris_drop_sums,ID=rep(1:50, 3) )
+iris2b <- iris_with_ID %>%
+  gather("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width", key="measure",value="value")
 #    Εναλλακτικά, μπορείτε να προσθέσετε στο iris2 την κατάλληλη αρίθμηση που να κάνει κάθε αρχική
 #    εγγραφή μοναδική. Παρατηρήστε ότι στο iris2 έχουμε 50 εγγραφές με Sepal.Length για τα setosa,
 #    ακολουθούμενες από 50 εγγραφές με Sepal.Length για τα versicolor, κοκ. Εφόσον ο συνδυασμός ενός
 #    ID (1,2,3,...,50) για κάθε είδος λουλουδιού με τον τύπο του λουλουδιού είναι μοναδικός για κάθε
 #    εγγραφή, αρκεί να προσθέσουμε στο iris2 μια νέα στήλη με περιεχόμενο 12 φορές το διάνυσμα 1:50.
-
+iris_with_ID_3<-mutate(iris_drop_sums,ID=rep(1:50, each=4,times=3) )
+iris3b <- iris_with_ID %>%
+  gather("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width", key="measure",value="value")
 #    Γενικά, για να δουλέψει το spead ή το pivot.wider, πρέπει κάθε set "απλωμένων" τιμών να αντιστοιχεί
 #    σε μοναδικό συνδυασμό τιμών των υπολοίπων στηλών.
 
@@ -123,8 +148,9 @@ df
 # 11  Ισμήνη   μεγ_πίεση visit2   140
 # 12  Ισμήνη μικρή_πίεση visit2    90
 
-
-
+df2 <- df %>%
+          gather("visit1","visit2",key="visit",value = "value")
+df2
 #14β Τώρα δημιουργείστε χωριστές στήλες για τη μεγάλη και τη μικρή πίεση.
 #   patient  visit μεγ_πίεση μικρή_πίεση
 # 1 Γιώργος visit1       170         100
@@ -133,3 +159,5 @@ df
 # 4  Ισμήνη visit2       140          90
 # 5  Νατάσα visit1       135          85
 # 6  Νατάσα visit2       110          70
+df3 <- df2 %>% spread(measure,value)
+df3
