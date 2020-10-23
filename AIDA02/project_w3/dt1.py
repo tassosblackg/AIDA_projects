aa3d199c2b530d02329337ed74cbf575a9493433
tassosblackg <soylis.k@gmail.com>
2020-10-23 16:36:40 +0300

    read .xlsx 2 dataframe,drop rows cleaning..

--- AIDA02/project_w3/dt1.py
+++ AIDA02/project_w3/dt1.py
@@ -6,5 +6,9 @@ from sklearn.metrics import confusion_matrix
 from sklearn.tree import DecisionTreeClassifier as DT
 
 # read titanic data set
-df = pd.read_excel("Data/Titanic.xlsx","Sheet1")
+df = pd.read_excel("Data/Titanic.xlsx","Sheet1",header=0)
+df = df.drop(df.index[0:2]) # drop two rows with nan data
+df = df.reset_index(drop=True) # re-arrange indeces stasting from 0,
 print(df.head)
+
+# split to input and labels
