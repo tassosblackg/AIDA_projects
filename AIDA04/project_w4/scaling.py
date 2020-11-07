b72d2f7a295ac853b27b2c7da39cfb05345c8347
tassosblackg <soylis.k@gmail.com>
2020-11-07 09:24:20 +0200

    sum of col,row nnz.remove count_nz

--- AIDA04/project_w4/scaling.py
+++ AIDA04/project_w4/scaling.py
@@ -1,30 +1,12 @@
 from read_mps import mps2data
 import numpy as np
 
-def count_nz(A):
-    row_nnz, col_nnz = [],[]
-    # nnz across rows
-    for i in range(A.shape[0]):
-        count_i = 0
-        for j in range(A.shape[1]):
-            if(A[i][j] != 0):
-                count_i = count_i + 1
-        row_nnz.append(count_i)
-
-    # nnz across cols
-    for j in range(A.shape[1]):
-        count_j = 0
-        for i in range(A.shape[0]):
-            if(A[i][j] != 0):
-                count_j = count_j + 1
-        col_nnz.append(count_j)
-
-    return row_nnz,col_nnz
 
 def scaleA(A,b,c):
 
     r,s = [],[]
-    row_nnz,col_nnz = count_nz(A)
+    row_nnz = (A!=0).sum(0)
+    col_nnz = (A!=0).sum(1)
     sum_row = np.sum(A,axis=1) # length num of rows
     sum_col = np.sum(A,axis=0) # length num of cols
     # calculate row scale factor
