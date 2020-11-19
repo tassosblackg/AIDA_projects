eb156dc836d43ee25a2df65f0f8561989b323d16
tassosblackg <soylis.k@gmail.com>
2020-11-19 21:42:28 +0200

    fix row,column indexing for all the arrays..

--- AIDA04/project_w5/read_mps.py
+++ AIDA04/project_w5/read_mps.py
@@ -1,41 +1,52 @@
 # first week read mps function fixed issues
+# rows and columns name must start from 1 inside .mps file
 # author @tassosblackg
 
 import numpy as np
+import re
 
 
-# search for string key in dictionary return index i of the position
-def get_dict_indx(my_dict,key2search):
-    for indx, key in enumerate(my_dict):
-        if(key2search in key):
-            break
-        else:
-            indx =-1 # not found
-    return indx
+def get_unique_cols(col_dict):
+    """
+    Create a list with columns names, from columns dictionary, dictionary has duplicants
+    Get the unique columns names
+    """
+    col_indices = []
+    for key in col_dict:
+        col_indices.append(key[0])
+    return(len(set(col_indices)))
+
+def convert_string_to_int(string):
+    return(int(re.search(r'\d+', string)[0]))
+
 
 # Get Amn matrix of coefficients
-def col2matrix(columns_dict,rows_dict):
+def col2matrix(columns_dict,len_rows_dict,num_unique_col):
     '''
         Get COLUMNS section data aka the constraints' coefficients
         @columns_dict: the dicitonary with columns section data
-        @rows_dict: the dictionary with rows data
+        @len_rows_dict: the length of dictionary with rows data
 
         @returns: A_mn array with the coefficients
     '''
 
-    # A = [ [0]*len(columns_dict) ]*len(rows_dict) #init M x N  array with 0
-    A = np.zeros((len(columns_dict),len(rows_dict)))
-    for indx_j, key in enumerate(columns_dict):
-        key_row_name = key[1] # get the 2d part of key_tuple => row_name
-        indx_i = get_dict_indx(rows_dict, key_row_name)
+    # print(columns_unique_names)
+    A = np.zeros((len_rows_dict,num_unique_col)) #  matrix
+    print(A.shape)
+    for  key in columns_dict:
+        # get the 2d part of key_tuple => row_name (because rows with different name in col section)
+        indx_i = convert_string_to_int(key[1]) -1  # get index for row i name
+        indx_j = convert_string_to_int(key[0]) -1 # get index for column j name -1 because starting counting from 0
+        # print(indx_i,indx_j)
         if (indx_i !=-1):
             A[indx_i][indx_j]=columns_dict[key] # get value from dictionary
         else:
             print("Error Wrong Name for Row inside Columns section Row doesn;t exist in ROWS section!\n");
+
     return A
 
 # Get objectives function coefficients dictionary and convert it to matrix
-def objectiveF_coef2matrix(columns_dict,obj_func_dict):
+def objectiveF_coef2matrix(columns_dict,obj_func_dict,num_unique_col):
     '''
         Get objective's function coefficients dictionary and convert it to matrix/vector
         @columns_dict : the dictionary of columns' section data
@@ -43,9 +54,10 @@ def objectiveF_coef2matrix(columns_dict,obj_func_dict):
 
         @returns : c array of objective function's coefficients
     '''
-    obj_coef = [0]*len(columns_dict)
+
+    obj_coef = [0]*num_unique_col
     for indx_c, key in enumerate(obj_func_dict):
-        indx = get_dict_indx(columns_dict, key)
+        indx = convert_string_to_int(key) -1 #indx starting from 0
         if(indx!=-1):
             obj_coef[indx] = obj_func_dict[key]
         else:
@@ -73,17 +85,17 @@ def rows2matrix(rows_dict):
 
     return Eq
 
-def rhs2matrix(rhs_dict,rows_dict):
+def rhs2matrix(rhs_dict,len_rows_dict):
     '''
         Convert RHS constraints covert dictionary to matrix
         @rhs_dict : right hand constraints values
-        @rows_dict: the dictionary with row's section data
+        @len_rows_dict: the length of dictionary with row's section data
 
         @returns : Eqin array
     '''
-    b=[0]*len(rows_dict)
+    b=[0]*len_rows_dict
     for key in rhs_dict:
-        indx = get_dict_indx(rows_dict, key)
+        indx = convert_string_to_int(key) -1
         if(indx != -1):
             b[indx] = rhs_dict[key]
         else:
@@ -141,21 +153,22 @@ def mps2data(file_name):
             elif(in_COLS_section and l[:3] != 'RHS'):
                 col_indx = l[4:13].strip() # Col_name
                 field4 = float(l[24:37].strip()) # Get value from field 4, convert to float
-#                    Has fields 5,6
-                if(len(l)>40):
-                    field5 = l[39:48].strip()
-                    field6 =float(l[50:62].strip())
 #                    ROW1 ,Value2 (Field3,Field4)
                 if( (l[14:23].strip() == obj_func_name) ): #<-is it constraint's row or objective's function??
                     obj[col_indx] = field4
                 else: # row_name is not objective_function
                     field3 = l[14:23].strip() # Row_name_1 -> indx
                     Cols[col_indx,field3] = field4
+#                    Has fields 5,6
+                if(len(l)>40):
+                    field5 = l[39:48].strip()
+                    field6 =float(l[50:62].strip())
+
 #                    ROW2 ,Value2 (Field5,Field6)
-                if((l[39:48].strip() == obj_func_name)):
-                    obj[col_indx]=float(l[50:62].strip())
-                else:
-                    Cols[col_indx,field5] = field6 #field6 value
+                    if((l[39:48].strip() == obj_func_name)):
+                        obj[col_indx]=float(l[50:62].strip())
+                    else:
+                            Cols[col_indx,field5] = field6 #field6 value
 #
 # #--------------------------------------------------| RHS section |-----------------------------------------------------------------------
             elif(l[:3] == 'RHS'):
@@ -186,12 +199,15 @@ def mps2data(file_name):
 
         f.close()
 
-    c = objectiveF_coef2matrix(Cols, obj)
-    A = col2matrix(Cols,Rows)
+    num_unique_col =get_unique_cols(Cols) # column index is appeared multiple times
+    # convert objective's function dictionary to an array/list
+    c = objectiveF_coef2matrix(Cols, obj,num_unique_col)
+    # convert coefficients dictionary to array/list
+    A = col2matrix(Cols,len(Rows),num_unique_col)
 #------------------ Convert ['E', 'G', 'L'] -> [0,1,-1] ----------------------------------------------------------------------------------
     Eq=rows2matrix(Rows)
 #------------------- Convert right-hand constraints to array missing row values =zero
-    b = rhs2matrix(RHS,Rows)
+    b = rhs2matrix(RHS,len(Rows))
 #------------------------------- RHS vector from 1xN to Nx1 format Transpose--------------------------------------------
     min_max = 1
     b_m = np.array(b)
