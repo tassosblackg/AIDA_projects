229d2f5901075f2a887a466ddd2ad662ac870b60
tassosblackg <soylis.k@gmail.com>
2020-12-08 17:06:46 +0200

    rename & add parser

--- AIDA04/project_w8/tsp_data_generator.py
+++ AIDA04/project_w8/tsp_data_generator.py
@@ -1,9 +1,11 @@
 # Generate Random Data
-# for TSP problem and for Knapsack
+# for TSP problem
 # author: @tassosblackg
 import argparse
 import numpy as np
+
 DATA_TAG = 'NODE_COORD_SECTION'
+
 def tsp_coord_data(numOfRecords,range_start,range_end):
     # numOfRecordsx2 array, N records with X,Y columns
     coord_xy = np.random.uniform(range_start, range_end, [numOfRecords,2]).round(3)
@@ -23,5 +25,22 @@ def coord_array2txt(array):
 
 
 
-mat=tsp_coord_data(20, 12.3, 1020.32)
-coord_array2txt(mat)
+# mat=tsp_coord_data(20, 12.3, 1020.32)
+# coord_array2txt(mat)
+# parser menu
+def parserM():
+
+    parser=argparse.ArgumentParser(prog="Knapsack read data")
+    parser.add_argument('numOfRecords',type=int,help='<Give Number of Records integer>')
+    parser.add_argument('start_r',type=int,help='<Give range_start integer>')
+    parser.add_argument('end_r',type=int,help='<Give range_end integer>')
+
+    args=parser.parse_args()
+
+    # Get numpy Array
+    mat = tsp_coord_data(args.numOfRecords,args.start_r,args.end_r)
+    coord_array2txt(mat)
+
+
+if __name__ == '__main__':
+    parserM()
