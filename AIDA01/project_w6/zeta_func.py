1455e9c35f07c6bbde99302910b0e863d13f2cb3
tassosblackg <soylis.k@gmail.com>
2020-12-09 20:54:26 +0200

    fix error mult no sum

--- AIDA01/project_w6/zeta_func.py
+++ AIDA01/project_w6/zeta_func.py
@@ -24,9 +24,9 @@ za = zeta(u+mat[0])
 zb = zeta(u+mat[1])
 z1 = zeta(u)
 zab = zeta(u+mat[0]+mat[1])
-p12_dif = za+zb
+p12_dif = za*zb
 p12_sam = z1*zab
-print("Persons 1-2 bayes factor = ",p12_dif,p12_sam,'\n')
+print("Persons 1-2 bayes factor = ",'(',p12_dif,p12_sam,')','/=',p12_dif/p12_sam,'\n')
 #8.368188175996467e-10 2.4202096393388754e-21
 
 #persons 1,3
@@ -34,9 +34,9 @@ za = zeta(u+mat[0])
 zc = zeta(u+mat[2])
 
 zac = zeta(u+mat[0]+mat[2])
-p13_dif = za+zc
+p13_dif = za*zc
 p13_sam = z1*zac
-print("Persons 1-3 bayes factor = ",p13_dif/p13_sam,'\n')
+print("Persons 1-3 bayes factor = ",'(',p13_dif,p13_sam,')','/=',p13_dif/p13_sam,'\n')
 
 
 # persons2,3
@@ -44,14 +44,19 @@ zb = zeta(u+mat[1])
 zc = zeta(u+mat[2])
 
 zbc = zeta(u+mat[1]+mat[2])
-p23_dif = zb+zc
+p23_dif = zb*zc
 p23_sam = z1*zbc
-print("Persons 2-3 bayes factor = ",p23_dif/p23_sam,'\n')
+print("Persons 2-3 bayes factor = ",'(',p23_dif,p23_sam,')','/=',p23_dif/p23_sam,'\n')
 
 
 
 
 zabc = zeta(u+mat[0]+mat[1]+mat[2])
-p123_dif = za+zb+zc
+p123_dif = za*zb*zc
 p123_sam = z1*zabc
-print("Persons 1-2-3 bayes factor = ",p123_dif/p123_sam,'\n')
+print("Persons 1-2-3 bayes factor = ",'(',p123_dif,p123_sam,')','/=',p123_dif/p123_sam,'\n')
+
+print('u+a = [ ',u+mat[0],'] ,','u+b= [ ',u+mat[1],'] u+c= [',u+mat[2],']\n')
+print('u+a+b = [ ',u+mat[0]+mat[1],']\n' )
+print('u+a+c = [ ',u+mat[0]+mat[2],']\n' )
+print('u+b+c = [ ',u+mat[1]+mat[2],']\n' )
