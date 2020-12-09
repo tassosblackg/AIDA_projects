a9b28c0a1cb25f10e701b6e8e43486a33f0cbdb4
tassosblackg <soylis.k@gmail.com>
2020-12-09 22:00:45 +0200

    add code for monte-carlo approxim

--- AIDA01/project_w6/pHrand_func.py
+++ AIDA01/project_w6/pHrand_func.py
@@ -1,8 +1,10 @@
 # HW 6 PGMs
 # Problem 12.5.4 calculations
 # author: @tassosblackg
-
 from scipy.special import beta
+import random
+from statistics import stdev,mean
+from math import sqrt
 
 a,b = 1,1
 
@@ -25,3 +27,30 @@ pHr2 = p_random_H(R2, W2, a, b)
 
 print('For R=10,w=12 = ',pHr1,'\n')
 print('For R=100,w=120 = ',pHr2,'\n')
+
+# Trying to prove the 12.5.5 question by using a
+# Monte Carlo experiments, approach  by calculating the mean values
+# for N iterations with different Values of R and W
+# and at the end the distance must be as small as possible
+# N equal to the number of R,W values
+# For large values of R,W beta gives nan
+def converge_std():
+     R = random.sample(range(10, 130), 100)
+    #R = [1,10,100,140,180]
+    W = random.sample(range(min(R)+15, max(R)+15), 100)
+    #W = [2,12,120,160,200]
+    val_equation, approx_val = [],[]
+    for i in range(len(R)):
+        val_equation.append(p_random_H(R[i], W[i], 1, 1))
+        approx_val.append(0.5*sqrt(R[i]+W[i])) # std value given the type
+
+    print(val_equation)
+    std_equation = stdev(val_equation)
+
+    std_approx = mean(approx_val)
+
+    print('Stdev using equation from (12.5.4) = ',std_equation,'\n')
+    print('Stdev using approximation type of (12.5.5) = ',std_approx,'\n')
+    print('Stdev Absolute Distance between two stds = ',abs(std_equation - std_approx),'\n')
+
+converge_std()
