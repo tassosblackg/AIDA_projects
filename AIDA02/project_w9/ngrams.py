a09bd3cf2360e27a11eb421dd80a03e34a066b33
tassosblackg <soylis.k@gmail.com>
2020-12-15 20:24:49 +0200

    calculate freq bigrams,trigrams

--- AIDA02/project_w9/ngrams.py
+++ AIDA02/project_w9/ngrams.py
@@ -5,23 +5,35 @@ import nltk
 from nltk.corpus import gutenberg as gut
 from nltk.text import Text
 from nltk.tokenize import sent_tokenize,word_tokenize
+from nltk.util import ngrams,trigrams,bigrams
+import random
+from nltk.probability import ConditionalFreqDist as CFD
 
-
+# ----------------
+#   Question A
+# ----------------
 # Get all books' titles of Gutenberg Corpus
 books = gut.fileids()
-print(books,len(books))
+# print(books,len(books))
 # keep only the first Ten of them
 books_10 = books[0:10]
-print(books_10)
+print("\nFirst 10 Books Titles: \n",books_10)
 
 
-
-# Creates a lsit of ten different vocabularies for each book
-vocabulary_per_book,vocabulary2 = [],[]
+# ----------------
+#   Question B
+# ----------------
+# Keep whole text of 10 books into a list
+book_full_txt = []
 for i in books_10:
-    # print(i)
     book_txt = gut.raw(i)
+    book_full_txt.append(book_txt)
+
+# Creates a list of ten different vocabularies for each book
+vocabulary_per_book,vocabulary_final,books_all_words = [],[],[]
+for book_txt in book_full_txt:
     words_of_book = word_tokenize(book_txt) # split text to words
+    books_all_words += words_of_book # keep all books words in a list
     vocabulary_per_book += set(words_of_book) # add list of unique words per book
 # remove dublicate words between books
 vocabulary_final += set(vocabulary_per_book)
@@ -29,12 +41,43 @@ vocabulary_final += set(vocabulary_per_book)
 print("\n Length of Vocaboluary as merged words from each book = ",len(vocabulary_per_book))
 print("\n Length of vocabulary after removing dublicate words beetween books = ",len(vocabulary_final))
 
+# ------------------
+#     Question C
+# ------------------
+sentences = []
+token = '<s>'
+for book_txt in book_full_txt:
+    for sent in sent_tokenize(book_txt):
+        new_sentence = token+sent+token
+    sentences.append(new_sentence)
+print("The number of total sentences is", len(sentences))
+print(sentences)
 
-# sents = nltk.sent_tokenize(list1)
-# print("The number of sentences is", len(sents),type(sents))
-# words = nltk.word_tokenize(list1)
-# print("The number of tokens is", len(words),type(words))
-# average_tokens = round(len(words)/len(sents))
-# print("The average number of tokens per sentence is",average_tokens)
-# unique_tokens = set(words)
-# print("The number of unique tokens are", len(unique_tokens))
+# ------------------
+#     Question D
+# ------------------
+books_words_unigram = ngrams(books_all_words, 1, pad_left=True, pad_right=True, left_pad_symbol=token, right_pad_symbol=token)
+unigram_fd = nltk.FreqDist(books_words_unigram)
+print(unigram_fd.keys())
+
+bigram_fd = nltk.FreqDist(ngrams(books_all_words, 2, pad_left=True, pad_right=True, left_pad_symbol=token, right_pad_symbol=token))
+print(bigram_fd.keys())
+
+trigram_fd = nltk.FreqDist(ngrams(books_all_words, 3, pad_left=True, pad_right=True, left_pad_symbol=token, right_pad_symbol=token))
+print(trigram_fd.keys())
+print(trigram_fd['is'])
+
+
+# ------------------
+#     Question E
+# # ------------------
+# # peek a random word and generate 10 sentences
+# def generate_model(cfdist, word, num=15):
+#     for i in range(num):
+#         words = list(cfdist[word])
+#         word = random.choice(words)
+#         print(word,file=open('output.txt', 'a'))
+#
+#
+# for i in range(0,10):
+#     generate_model(bigram_fd, 'The', num=15)
