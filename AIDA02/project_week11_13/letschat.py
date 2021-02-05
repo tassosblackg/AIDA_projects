cda2c9bb1aefde7e783591841fb3acc5b129431d
tassosblackg <soylis.k@gmail.com>
2021-02-05 21:23:56 +0200

    shit uploadd

--- AIDA02/project_week11_13/letschat.py
+++ AIDA02/project_week11_13/letschat.py
@@ -171,7 +171,7 @@ def str_to_tokens( sentence : str ):
 
 # --- Step 1 - Load Data as dataFrame
 total_raws = 238051
-df = pd.read_csv('output.tsv',sep='\t',nrows=8000) # a csv like format with headers Questions , Answers
+df = pd.read_csv('output.tsv',sep='\t',nrows=1000) # a csv like format with headers Questions , Answers
 print("\n \> Questions column-data ../\n",df.Questions)
 print(df.Questions[1],type(df.Questions[1]))
 
@@ -237,37 +237,37 @@ model = tf.keras.models.Model([encoder_inputs, decoder_inputs], output )
 model.compile(optimizer=tf.keras.optimizers.Adam(), loss='categorical_crossentropy')
 
 model.summary() # summary of the model params,architecture
-# utils.plot_model(model) # model architecture
+utils.plot_model(model) # model architecture
 
-#Train model
-model.fit([encoder_input_data , decoder_input_data], decoder_output_data, batch_size=50, epochs=150 )
-# model.save( 'model8k1.h5' ) # save trained model
+# #Train model
+# model.fit([encoder_input_data , decoder_input_data], decoder_output_data, batch_size=50, epochs=150 )
+# # model.save( 'model8k1.h5' ) # save trained model
+# #
+# # model = load_model('model8k1.h5')
+# enc_model , dec_model = make_inference_models()
 #
-# model = load_model('model8k1.h5')
-enc_model , dec_model = make_inference_models()
-
-
-# Talk with bot
-for _ in range(10):
-    states_values = enc_model.predict( str_to_tokens( input( 'Enter question : ' ) ) )
-    empty_target_seq = np.zeros( ( 1 , 1 ) )
-    empty_target_seq[0, 0] = tokenizer.word_index['start']
-    stop_condition = False
-    decoded_translation = ''
-    while not stop_condition :
-        dec_outputs , h , c = dec_model.predict([ empty_target_seq ] + states_values )
-        sampled_word_index = np.argmax( dec_outputs[0, -1, :] )
-        sampled_word = None
-        for word , index in tokenizer.word_index.items() :
-            if sampled_word_index == index :
-                decoded_translation += ' {}'.format( word )
-                sampled_word = word
-
-        if sampled_word == 'end' or len(decoded_translation.split()) > maxlen_answers:
-            stop_condition = True
-
-        empty_target_seq = np.zeros( ( 1 , 1 ) )
-        empty_target_seq[ 0 , 0 ] = sampled_word_index
-        states_values = [ h , c ]
-
-    print( decoded_translation )
+#
+# # Talk with bot
+# for _ in range(10):
+#     states_values = enc_model.predict( str_to_tokens( input( 'Enter question : ' ) ) )
+#     empty_target_seq = np.zeros( ( 1 , 1 ) )
+#     empty_target_seq[0, 0] = tokenizer.word_index['start']
+#     stop_condition = False
+#     decoded_translation = ''
+#     while not stop_condition :
+#         dec_outputs , h , c = dec_model.predict([ empty_target_seq ] + states_values )
+#         sampled_word_index = np.argmax( dec_outputs[0, -1, :] )
+#         sampled_word = None
+#         for word , index in tokenizer.word_index.items() :
+#             if sampled_word_index == index :
+#                 decoded_translation += ' {}'.format( word )
+#                 sampled_word = word
+#
+#         if sampled_word == 'end' or len(decoded_translation.split()) > maxlen_answers:
+#             stop_condition = True
+#
+#         empty_target_seq = np.zeros( ( 1 , 1 ) )
+#         empty_target_seq[ 0 , 0 ] = sampled_word_index
+#         states_values = [ h , c ]
+#
+#     print( decoded_translation )
